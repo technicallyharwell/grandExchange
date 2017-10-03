87864ea0b2ad622ec64ef3e84e5708ab2a787ced
Robert <20748700+robdoescode@users.noreply.github.com>
2017-10-03 18:05:38 -0500

    organized main routine into functions

--- updatePrices.py
+++ updatePrices.py
@@ -2,6 +2,58 @@ import MySQLdb
 import urllib, json
 import time
 
+def selectRunedate(cursor):
+    cursor.execute("""SELECT runedate FROM currentRunedate""")
+    runedate = cursor.fetchone()
+
+    return runedate
+
+def fetchRecentRunedate():
+    try:
+        url = "http://secure.runescape.com/m=itemdb_rs/api/info.json"
+        response = urllib.urlopen(url)
+        responseData = json.loads(response.read())
+        recentRunedate = responseData["lastConfigUpdateRuneday"]
+    except:
+        print "could not retrieve most recent Runedate...\n"
+
+    return recentRunedate
+
+def selectItemIDList(cursor):
+    try:                                                    #get the item ID of all items
+        cursor.execute("""SELECT iid FROM itemNames""")
+        data = curs.fetchall()
+        idList = []                                         #store each item ID into idList[]
+        for value in data:
+            idList.append(value[0])
+    except:
+        print "could not select iid from itemNames...\n"
+
+    return idList
+
+def updateItemPrices(idList, cursor):
+    for iid in idList:
+    #    print iid
+        try:
+            url = "http://services.runescape.com/m=itemdb_rs/api/graph/" + str(iid) + ".json"
+    #        print url
+            response = urllib.urlopen(url)
+            data = json.loads(response.read())    
+            dailyData = data["daily"]
+            dailyKeys = []
+            for k in dailyData:
+                dailyKeys.append(k)        
+            dailyKeys.sort()
+            lastKey = len(dailyKeys) - 1
+            recentPrice = dailyData[dailyKeys[lastKey]]
+            print "iid: ", iid, " with current price: ", recentPrice, "\n"
+            time.sleep(1)
+        except:
+            print "unable to connect to website...\n"
+
+    return
+
+
 myDB = "grandexchange"
 
 try:
@@ -14,40 +66,12 @@ try:
 except:
     print "could not connect to database " + myDB + "\n"
 
-try:                                                    #get the item ID of all items
-    curs.execute("""SELECT iid FROM itemNames""")
-    data = curs.fetchall()
-    idList = []                                         #store each item ID into idList[]
-    for value in data:
-        idList.append(value[0])
-except:
-    print "could not execute select statement...\n"
-
-
-
-for iid in idList:
-#    print iid
-    try:
-        url = "http://services.runescape.com/m=itemdb_rs/api/graph/" + str(iid) + ".json"
-#        print url
-        response = urllib.urlopen(url)
-        data = json.loads(response.read())    
-        dailyData = data["daily"]
-        dailyKeys = []
-        for k in dailyData:
-            dailyKeys.append(k)        
-        dailyKeys.sort()
-        lastKey = len(dailyKeys) - 1
-        recentPrice = dailyData[dailyKeys[lastKey]]
-        print "iid: ", iid, " with current price: ", recentPrice, "\n"
-        time.sleep(1)
-    except:
-        print "unable to connect to website...\n"
-
-
-
-
-
-
+savedRunedate = selectRunedate(curs)
+#print "runedate is: ", savedRunedate[0], "\n"
+recentRunedate = fetchRecentRunedate()
+#print "recent Runedate is: ", recentRunedate, "\n"
+if (savedRunedate[0] != recentRunedate):
+    itemIDs = selectItemIDList(curs)
+    updateItemPrices(itemIDs, curs)
 
 conn.close()
